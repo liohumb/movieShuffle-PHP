0e91e0468b85919dee7924accbdab1e1623e55d1
liohumb <helloliohumb@gmail.com>
2023-01-19 15:13:38 +0100

    correction de la home page avec ajout titre et genre, responsive home page & single movie page

--- index.php
+++ index.php
@@ -9,11 +9,15 @@
     <div class="home__container">
         <?php foreach ($movies as $movie) { ?>
             <div class="home__movie">
-                <a href="movie.php?id=<?= $movie['id'] ?>" class="home__movie-link">
-                    <img src="assets/images/poster/<?= str_replace(' ', '-', $movie['title']) ?>.jpg"
-                         alt="<?= $movie['title'] ?>"
-                         class="home__movie-poster">
-                </a>
+                <img src="assets/images/poster/<?= str_replace(' ', '-', $movie['title']) ?>.jpg"
+                     alt="<?= $movie['title'] ?>"
+                     class="home__movie-poster">
+                <div class="home__movie-infos">
+                    <a href="movie.php?id=<?= $movie['id'] ?>" class="home__movie-link">
+                        <h1 class="home__movie-infos--title"><?= $movie['title'] ?></h1>
+                    </a>
+                    <span class="home__movie-infos--gender"><?= implode(", ", $movie['genres']) ?></span>
+                </div>
             </div>
         <?php } ?>
     </div>
